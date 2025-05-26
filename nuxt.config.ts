8a83e5386667002a7e4d7e879c872751b77ce119
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-05-26 14:00:58 +0200

    🔧 Fixing supabase key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,9 @@ export default defineNuxtConfig({
   },
   supabase: {
     redirect: true,
+    key: process.env.SUPABASE_ANON_KEY || "",
+    url: process.env.SUPABASE_URL || "",
+    serviceKey: process.env.SUPABASE_SERVICE_ROLE_KEY || "",
     redirectOptions: {
       login: "/auth/login",
       callback: "/auth/confirm",
