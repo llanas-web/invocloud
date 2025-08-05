ee7d8a61c6c19a94d52921f0f122e9afe899e8c1
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-08-05 08:46:54 +0200

    🐛 Open /stripe/webhook api calls for stripe

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: "/auth/login",
       callback: "/auth/confirm",
-      exclude: ["/", "/auth/*"],
+      exclude: ["/", "/auth/*", "/stripe/webhook"],
     },
   },
   icon: {
