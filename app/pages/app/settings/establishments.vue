f3c7c40bde68ceca8ed2532a658fd521bae32264
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-09-22 23:30:01 +0200

    ✨ Adding establishment delete

--- app/pages/app/settings/establishments.vue
+++ app/pages/app/settings/establishments.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
-import { EstablishmentsMembersInviteForm } from '#components'
+  import { EstablishmentsMembersInviteForm } from '#components'
 
-const { selectedEstablishment } = useEstablishments()
+  const { selectedEstablishment, deleteEstablishment } = useEstablishments()
 
+  const onDeleteEstablishment = async () => {
+    const response = await deleteEstablishment()
+    if (response) {
+      useToast().add({
+        title: "L'établissement a été supprimé avec succès.",
+        color: "success",
+      })
+      // Redirect to home or establishments list page
+      useRouter().push('/app/settings/establishments');
+    } else {
+      useToast().add({
+        title: "Échec de la suppression de l'établissement",
+        color: "error",
+      })
+    }
+  }
 </script>
 
 <template>
@@ -19,6 +35,13 @@ const { selectedEstablishment } = useEstablishments()
       <USeparator />
       <EstablishmentsMembersInviteForm />
       <EstablishmentsMembersList />
+      <UPageCard title="Supprimer l'établissement"
+        description="Une fois une structure supprimée, toutes ses données seront définitivement supprimées. Avant de supprimer une structure, veuillez vous assurer que vous avez sauvegardé toutes les données importantes."
+        class="bg-gradient-to-tl from-error/10 from-5% to-default">
+        <template #footer>
+          <UButton label="Supprimer l'établissement" color="error" @click="onDeleteEstablishment" />
+        </template>
+      </UPageCard>
     </template>
   </div>
 </template>
