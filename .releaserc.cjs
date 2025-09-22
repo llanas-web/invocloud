dcc12d9ef0323d5447d616e4379d0bd54924ef6e
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-09-22 22:43:25 +0200

    💚 Fixing CI build commit on merge

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -51,8 +51,7 @@ module.exports = {
     ["@semantic-release/npm", { npmPublish: false }],
     ["@semantic-release/git", {
       assets: ["CHANGELOG.md", "package.json"],
-      message:
-        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      message: "🔖 ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
     }],
     ["@semantic-release/github", { assets: [] }],
   ],
