12b3b95b1be5d431480037989ea92586a80fff8b
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-10-02 21:22:29 +0200

    🗃️ Creating database migration file from 3 evolutions

---
+++ supabase/migrations/20251002192035_adding_zip_mail_ocr.sql
@@ -0,0 +1,563 @@
+revoke delete on table "public"."establishment_members" from "anon";
+
+revoke insert on table "public"."establishment_members" from "anon";
+
+revoke references on table "public"."establishment_members" from "anon";
+
+revoke select on table "public"."establishment_members" from "anon";
+
+revoke trigger on table "public"."establishment_members" from "anon";
+
+revoke truncate on table "public"."establishment_members" from "anon";
+
+revoke update on table "public"."establishment_members" from "anon";
+
+revoke delete on table "public"."establishment_members" from "authenticated";
+
+revoke insert on table "public"."establishment_members" from "authenticated";
+
+revoke references on table "public"."establishment_members" from "authenticated";
+
+revoke select on table "public"."establishment_members" from "authenticated";
+
+revoke trigger on table "public"."establishment_members" from "authenticated";
+
+revoke truncate on table "public"."establishment_members" from "authenticated";
+
+revoke update on table "public"."establishment_members" from "authenticated";
+
+revoke delete on table "public"."establishment_members" from "service_role";
+
+revoke insert on table "public"."establishment_members" from "service_role";
+
+revoke references on table "public"."establishment_members" from "service_role";
+
+revoke select on table "public"."establishment_members" from "service_role";
+
+revoke trigger on table "public"."establishment_members" from "service_role";
+
+revoke truncate on table "public"."establishment_members" from "service_role";
+
+revoke update on table "public"."establishment_members" from "service_role";
+
+revoke delete on table "public"."establishments" from "anon";
+
+revoke insert on table "public"."establishments" from "anon";
+
+revoke references on table "public"."establishments" from "anon";
+
+revoke select on table "public"."establishments" from "anon";
+
+revoke trigger on table "public"."establishments" from "anon";
+
+revoke truncate on table "public"."establishments" from "anon";
+
+revoke update on table "public"."establishments" from "anon";
+
+revoke delete on table "public"."establishments" from "authenticated";
+
+revoke insert on table "public"."establishments" from "authenticated";
+
+revoke references on table "public"."establishments" from "authenticated";
+
+revoke select on table "public"."establishments" from "authenticated";
+
+revoke trigger on table "public"."establishments" from "authenticated";
+
+revoke truncate on table "public"."establishments" from "authenticated";
+
+revoke update on table "public"."establishments" from "authenticated";
+
+revoke delete on table "public"."establishments" from "service_role";
+
+revoke insert on table "public"."establishments" from "service_role";
+
+revoke references on table "public"."establishments" from "service_role";
+
+revoke select on table "public"."establishments" from "service_role";
+
+revoke trigger on table "public"."establishments" from "service_role";
+
+revoke truncate on table "public"."establishments" from "service_role";
+
+revoke update on table "public"."establishments" from "service_role";
+
+revoke delete on table "public"."invoices" from "anon";
+
+revoke insert on table "public"."invoices" from "anon";
+
+revoke references on table "public"."invoices" from "anon";
+
+revoke select on table "public"."invoices" from "anon";
+
+revoke trigger on table "public"."invoices" from "anon";
+
+revoke truncate on table "public"."invoices" from "anon";
+
+revoke update on table "public"."invoices" from "anon";
+
+revoke delete on table "public"."invoices" from "authenticated";
+
+revoke insert on table "public"."invoices" from "authenticated";
+
+revoke references on table "public"."invoices" from "authenticated";
+
+revoke select on table "public"."invoices" from "authenticated";
+
+revoke trigger on table "public"."invoices" from "authenticated";
+
+revoke truncate on table "public"."invoices" from "authenticated";
+
+revoke update on table "public"."invoices" from "authenticated";
+
+revoke delete on table "public"."invoices" from "service_role";
+
+revoke insert on table "public"."invoices" from "service_role";
+
+revoke references on table "public"."invoices" from "service_role";
+
+revoke select on table "public"."invoices" from "service_role";
+
+revoke trigger on table "public"."invoices" from "service_role";
+
+revoke truncate on table "public"."invoices" from "service_role";
+
+revoke update on table "public"."invoices" from "service_role";
+
+revoke delete on table "public"."suppliers" from "anon";
+
+revoke insert on table "public"."suppliers" from "anon";
+
+revoke references on table "public"."suppliers" from "anon";
+
+revoke select on table "public"."suppliers" from "anon";
+
+revoke trigger on table "public"."suppliers" from "anon";
+
+revoke truncate on table "public"."suppliers" from "anon";
+
+revoke update on table "public"."suppliers" from "anon";
+
+revoke delete on table "public"."suppliers" from "authenticated";
+
+revoke insert on table "public"."suppliers" from "authenticated";
+
+revoke references on table "public"."suppliers" from "authenticated";
+
+revoke select on table "public"."suppliers" from "authenticated";
+
+revoke trigger on table "public"."suppliers" from "authenticated";
+
+revoke truncate on table "public"."suppliers" from "authenticated";
+
+revoke update on table "public"."suppliers" from "authenticated";
+
+revoke delete on table "public"."suppliers" from "service_role";
+
+revoke insert on table "public"."suppliers" from "service_role";
+
+revoke references on table "public"."suppliers" from "service_role";
+
+revoke select on table "public"."suppliers" from "service_role";
+
+revoke trigger on table "public"."suppliers" from "service_role";
+
+revoke truncate on table "public"."suppliers" from "service_role";
+
+revoke update on table "public"."suppliers" from "service_role";
+
+revoke delete on table "public"."upload_validations" from "anon";
+
+revoke insert on table "public"."upload_validations" from "anon";
+
+revoke references on table "public"."upload_validations" from "anon";
+
+revoke select on table "public"."upload_validations" from "anon";
+
+revoke trigger on table "public"."upload_validations" from "anon";
+
+revoke truncate on table "public"."upload_validations" from "anon";
+
+revoke update on table "public"."upload_validations" from "anon";
+
+revoke delete on table "public"."upload_validations" from "authenticated";
+
+revoke insert on table "public"."upload_validations" from "authenticated";
+
+revoke references on table "public"."upload_validations" from "authenticated";
+
+revoke select on table "public"."upload_validations" from "authenticated";
+
+revoke trigger on table "public"."upload_validations" from "authenticated";
+
+revoke truncate on table "public"."upload_validations" from "authenticated";
+
+revoke update on table "public"."upload_validations" from "authenticated";
+
+revoke delete on table "public"."upload_validations" from "service_role";
+
+revoke insert on table "public"."upload_validations" from "service_role";
+
+revoke references on table "public"."upload_validations" from "service_role";
+
+revoke select on table "public"."upload_validations" from "service_role";
+
+revoke trigger on table "public"."upload_validations" from "service_role";
+
+revoke truncate on table "public"."upload_validations" from "service_role";
+
+revoke update on table "public"."upload_validations" from "service_role";
+
+revoke delete on table "public"."user_settings" from "anon";
+
+revoke insert on table "public"."user_settings" from "anon";
+
+revoke references on table "public"."user_settings" from "anon";
+
+revoke select on table "public"."user_settings" from "anon";
+
+revoke trigger on table "public"."user_settings" from "anon";
+
+revoke truncate on table "public"."user_settings" from "anon";
+
+revoke update on table "public"."user_settings" from "anon";
+
+revoke delete on table "public"."user_settings" from "authenticated";
+
+revoke insert on table "public"."user_settings" from "authenticated";
+
+revoke references on table "public"."user_settings" from "authenticated";
+
+revoke select on table "public"."user_settings" from "authenticated";
+
+revoke trigger on table "public"."user_settings" from "authenticated";
+
+revoke truncate on table "public"."user_settings" from "authenticated";
+
+revoke update on table "public"."user_settings" from "authenticated";
+
+revoke delete on table "public"."user_settings" from "service_role";
+
+revoke insert on table "public"."user_settings" from "service_role";
+
+revoke references on table "public"."user_settings" from "service_role";
+
+revoke select on table "public"."user_settings" from "service_role";
+
+revoke trigger on table "public"."user_settings" from "service_role";
+
+revoke truncate on table "public"."user_settings" from "service_role";
+
+revoke update on table "public"."user_settings" from "service_role";
+
+revoke delete on table "public"."users" from "anon";
+
+revoke insert on table "public"."users" from "anon";
+
+revoke references on table "public"."users" from "anon";
+
+revoke select on table "public"."users" from "anon";
+
+revoke trigger on table "public"."users" from "anon";
+
+revoke truncate on table "public"."users" from "anon";
+
+revoke update on table "public"."users" from "anon";
+
+revoke delete on table "public"."users" from "authenticated";
+
+revoke insert on table "public"."users" from "authenticated";
+
+revoke references on table "public"."users" from "authenticated";
+
+revoke select on table "public"."users" from "authenticated";
+
+revoke trigger on table "public"."users" from "authenticated";
+
+revoke truncate on table "public"."users" from "authenticated";
+
+revoke update on table "public"."users" from "authenticated";
+
+revoke delete on table "public"."users" from "service_role";
+
+revoke insert on table "public"."users" from "service_role";
+
+revoke references on table "public"."users" from "service_role";
+
+revoke select on table "public"."users" from "service_role";
+
+revoke trigger on table "public"."users" from "service_role";
+
+revoke truncate on table "public"."users" from "service_role";
+
+revoke update on table "public"."users" from "service_role";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.check_sender_authorized(recipient_email text, sender_email text)
+ RETURNS TABLE(supplier_id uuid, establishment_id uuid)
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  return query
+  select suppliers.id as supplier_id, establishments.id as establishment_id
+  from public.suppliers
+  join public.establishments
+    on establishments.id = suppliers.establishment_id
+  join public.establishment_members
+    on establishment_members.establishment_id = establishments.id
+  join public.users
+    on establishment_members.user_id = users.id
+  where users.email = recipient_email
+  and sender_email = any(suppliers.emails);
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.establishment_has_active_subscription(establishment_id uuid)
+ RETURNS boolean
+ LANGUAGE sql
+ STABLE
+AS $function$
+  select coalesce(e.subscription_status in ('trialing','active','past_due','unpaid'), false)
+  from public.establishments e
+  where e.id = establishment_id
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.handle_new_establishment()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$BEGIN
+  INSERT INTO public.establishment_members (establishment_id, user_id, status, role)
+  VALUES (
+    NEW.id,
+    NEW.creator_id,
+    'accepted',
+    'admin'
+  );
+  RETURN NEW;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$DECLARE
+  new_establishment_id uuid;
+BEGIN
+  IF (NEW.is_anonymous = false) THEN
+    -- Insert into users table
+    INSERT INTO public.users (id, email, full_name)
+    VALUES (
+      NEW.id,
+      NEW.email,
+      NEW.raw_user_meta_data->>'full_name'
+    );
+
+    -- Handle invitation-based join
+    IF (NEW.raw_user_meta_data->>'establishment_id') IS NOT NULL THEN
+      new_establishment_id := (NEW.raw_user_meta_data->>'establishment_id')::uuid;
+
+      INSERT INTO public.establishment_members (establishment_id, user_id)
+      VALUES (
+        new_establishment_id,
+        NEW.id
+      );
+
+    -- Handle creation of a new establishment
+    ELSIF (NEW.raw_user_meta_data->>'establishment_name') IS NOT NULL THEN
+      INSERT INTO public.establishments (name, creator_id)
+      VALUES (
+        NEW.raw_user_meta_data->>'establishment_name',
+        NEW.id
+      )
+      RETURNING id INTO new_establishment_id;
+    END IF;
+
+    -- Insert user settings with favorite establishment
+    INSERT INTO public.user_settings (user_id, favorite_establishment_id)
+    VALUES (
+      NEW.id,
+      new_establishment_id
+    );
+  END IF;
+
+  RETURN NEW;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_auth_member_of_establishment(_establishment_id uuid, _accepted_only boolean)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$declare
+  _res boolean;
+begin
+  select exists (
+    select 1
+    from public.establishment_members em
+    where em.establishment_id = _establishment_id
+      and em.user_id = auth.uid()
+      and (_accepted_only is false or em.status = 'accepted')
+  )
+  into _res;
+
+  return coalesce(_res, false);
+end;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_auth_member_of_supplier(_supplier_id uuid, _accepted_only boolean)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$declare
+  _res boolean;
+begin
+  select exists (
+    select 1
+    from public.suppliers s
+    join public.establishment_members em
+      on em.establishment_id = s.establishment_id
+    where s.id = _supplier_id
+      and em.user_id = auth.uid()
+      and (_accepted_only is false or em.status = 'accepted')
+  )
+  into _res;
+
+  return coalesce(_res, false);
+end;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_auth_user_allowed_establishment(_establishment_id uuid)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$BEGIN
+  RETURN EXISTS(
+    SELECT 1
+    FROM public.establishments
+    WHERE establishments.creator_id = auth.uid()
+  );
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_auth_user_establishment_creator(_establishment_id uuid)
+ RETURNS boolean
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$DECLARE 
+  establishment_count int;
+
+BEGIN
+  RAISE LOG 'Is % the creator of %', auth.uid(), _establishment_id;
+  
+  SELECT COUNT(*) INTO establishment_count    
+  FROM establishments
+  WHERE challenges.id = _establishment_id AND challenges.creator_id = auth.uid();
+  
+  RAISE LOG 'There is % establishment', challenge_count;
+
+  RETURN establishment_count > 0;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_user_same_establishment(_user_a uuid, _user_b uuid, _accepted_only boolean)
+ RETURNS boolean
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$DECLARE _res boolean;
+BEGIN
+select exists (
+    select 1
+    from public.establishment_members em1
+    join public.establishment_members em2
+      on em1.establishment_id = em2.establishment_id
+    where em1.user_id = _user_a
+      and em2.user_id = _user_b
+      and (_accepted_only is false
+           or (em1.status = 'accepted' and em2.status = 'accepted'))
+  )
+  into _res;
+
+  return coalesce(_res, false);
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.on_upload_file()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$DECLARE
+  selected_supplier_id uuid;
+BEGIN
+  IF (NEW.status = 'uploaded') THEN
+
+    SELECT id
+    INTO selected_supplier_id
+    FROM public.suppliers
+    WHERE suppliers.establishment_id = NEW.selected_establishment
+      AND suppliers.id = ANY(NEW.suppliers); -- assuming NEW.suppliers is uuid[]
+
+    INSERT INTO public.invoices (
+      id,
+      supplier_id,
+      comment,
+      file_path,
+      name
+    ) VALUES (
+      NEW.id, 
+      selected_supplier_id,
+      NEW.comment,
+      NEW.file_path,
+      NEW.file_name
+    );
+
+  END IF;
+
+  NEW.status = 'done';
+  RETURN NEW;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.prevent_delete_establishment_if_active()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+begin
+  if public.establishment_has_active_subscription(old.id) then
+    raise exception 'Impossible de supprimer cet établissement: un abonnement Stripe est encore actif. Annulez l’abonnement puis réessayez.';
+  end if;
+  return old;
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.prevent_delete_user_if_active_sub()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+begin
+  if public.user_has_active_billing_establishments(old.id) then
+    raise exception 'Impossible de supprimer votre compte: au moins un de vos établissements a un abonnement Stripe actif. Annulez d’abord l’abonnement via la page Abonnement, puis réessayez.';
+  end if;
+  return old;
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.user_has_active_billing_establishments(user_id uuid)
+ RETURNS boolean
+ LANGUAGE sql
+ STABLE
+AS $function$
+  select exists (
+    select 1
+    from public.establishments e
+    where e.creator_id = user_id
+      and public.establishment_has_active_subscription(e.id)
+  )
+$function$
+;
+
+
