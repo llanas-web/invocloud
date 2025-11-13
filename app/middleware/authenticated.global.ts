5af33ea2e79e317246410f7964a4e22e3366cf2c
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-11-13 16:58:15 +0100

    ♻️REfactoring auth services + fixes

--- app/middleware/authenticated.global.ts
+++ app/middleware/authenticated.global.ts
@@ -1,13 +1,11 @@
 export default defineNuxtRouteMiddleware((middleware) => {
-    if (import.meta.client) {
-        const { $authRepository } = useNuxtApp();
-        if (middleware.path === "/") return;
-        if (!$authRepository.connectedUser) {
-            if (!middleware.path.startsWith("/auth")) {
-                return navigateTo("/auth/login");
-            }
-        } else {
-            if (middleware.path.startsWith("/auth")) return navigateTo("/app");
+    const { $authRepository } = useNuxtApp();
+    if (middleware.path === "/") return;
+    if (!$authRepository.connectedUser) {
+        if (!middleware.path.startsWith("/auth")) {
+            return navigateTo("/auth/login");
         }
+    } else {
+        if (middleware.path.startsWith("/auth")) return navigateTo("/app");
     }
 });
