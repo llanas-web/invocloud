2457e72934e58378f3b74837d55b43aa984fb639
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2026-01-15 06:25:37 +0100

    ✨ Adding url filters param on accept pending_invoices

--- app/components/invoices/upload/table.vue
+++ app/components/invoices/upload/table.vue
@@ -1,97 +1,101 @@
 <script setup lang="ts">
-    import type { TableColumn } from '@nuxt/ui'
-    import { formatDate } from '~/utils/date'
-    import { InvoiceStatus } from '~~/shared/domain/invoice/invoice.model'
+import type { TableColumn } from '@nuxt/ui'
+import { formatDate } from '~/utils/date'
+import { InvoiceStatus } from '~~/shared/domain/invoice/invoice.model'
 
-    const pendingInvoiceStatus: InvoiceStatus[] = [InvoiceStatus.DRAFT, InvoiceStatus.OCR, InvoiceStatus.PENDING]
+const pendingInvoiceStatus: InvoiceStatus[] = [InvoiceStatus.DRAFT, InvoiceStatus.OCR, InvoiceStatus.PENDING]
 
-    const { invoices } = useInvoices()
-    const { error: deleteInvoiceError, selectedInvoices, onSubmit } = useInvoicesDelete()
+const { invoices } = useInvoices()
+const { error: deleteInvoiceError, selectedInvoices, onSubmit } = useInvoicesDelete()
 
-    const UBadge = resolveComponent('UBadge')
-    const UButton = resolveComponent('UButton')
-    const toast = useToast()
+const UBadge = resolveComponent('UBadge')
+const UButton = resolveComponent('UButton')
+const toast = useToast()
+const route = useRoute();
 
-    const pendingInvoices = computed(() =>
-        invoices.value.filter((invoice) => pendingInvoiceStatus.includes(invoice.status))
-    )
+const pendingInvoices = computed(() =>
+    invoices.value.filter((invoice) => pendingInvoiceStatus.includes(invoice.status))
+)
 
-    const columns: TableColumn<typeof invoices.value[0]>[] = [
-        {
-            accessorKey: 'supplier',
-            header: 'Fournisseur',
-            cell: ({ row, table }) => {
-                return h('div', { class: 'flex items-center gap-3' }, [
-                    h('div', undefined, [
-                        h('p', { class: 'font-medium text-highlighted' }, row.original.supplierName),
-                    ])
+const columns: TableColumn<typeof invoices.value[0]>[] = [
+    {
+        accessorKey: 'supplier',
+        header: 'Fournisseur',
+        cell: ({ row, table }) => {
+            return h('div', { class: 'flex items-center gap-3' }, [
+                h('div', undefined, [
+                    h('p', { class: 'font-medium text-highlighted' }, row.original.supplierName),
                 ])
-            }
-        },
-        {
-            accessorKey: 'comment',
-            header: 'Commentaire',
-            cell: ({ row }) => {
-                return h('span', { class: 'font-medium text-highlighted' },
-                    row.getValue('comment')
-                )
-            }
-        },
-        {
-            accessorKey: 'created_at',
-            header: 'Envoyé le',
-            cell: ({ row }) => {
-                return formatDate(row.original.emitDate);
-            }
-        },
-        {
-            accessorKey: 'status',
-            header: 'Status',
-            cell: ({ row }) => {
-                return h(UBadge, { variant: 'subtle', color: 'neutral' }, () => {
-                    switch (row.original.status) {
-                        case 'draft':
-                            return 'En attente de soumission';
-                        case 'ocr':
-                            return 'En cours de traitement OCR';
-                        case 'pending':
-                            return 'En attente de validation';
-                        default:
-                            return row.original.status;
-                    }
-                })
-            }
-        },
-        {
-            accessorKey: 'Valider',
-            header: '',
-            cell: ({ row }) => {
-                return (row.original.status === 'pending') ? h('div', { class: 'flex justify-end items-center gap-3' }, [
-                    h(UButton, {
-                        color: 'success',
-                        icon: 'i-lucide-check',
-                        size: 'sm',
-                        class: 'cursor-pointer',
-                        label: 'Accepter',
-                        onClick: () => {
-                            return navigateTo(`/app/invoices/${row.original.id}`)
-                        }
-                    }),
-                    h(UButton, {
-                        color: 'error',
-                        icon: 'i-lucide-x',
-                        size: 'sm',
-                        class: 'cursor-pointer',
-                        label: 'Refuser',
-                        onClick: async () => {
-                            selectedInvoices.value = [row.original.id]
-                            await onSubmit()
-                        }
-                    }),
-                ]) : null
-            }
+            ])
         }
-    ]
+    },
+    {
+        accessorKey: 'comment',
+        header: 'Commentaire',
+        cell: ({ row }) => {
+            return h('span', { class: 'font-medium text-highlighted' },
+                row.getValue('comment')
+            )
+        }
+    },
+    {
+        accessorKey: 'created_at',
+        header: 'Envoyé le',
+        cell: ({ row }) => {
+            return formatDate(row.original.emitDate);
+        }
+    },
+    {
+        accessorKey: 'status',
+        header: 'Status',
+        cell: ({ row }) => {
+            return h(UBadge, { variant: 'subtle', color: 'neutral' }, () => {
+                switch (row.original.status) {
+                    case 'draft':
+                        return 'En attente de soumission';
+                    case 'ocr':
+                        return 'En cours de traitement OCR';
+                    case 'pending':
+                        return 'En attente de validation';
+                    default:
+                        return row.original.status;
+                }
+            })
+        }
+    },
+    {
+        accessorKey: 'Valider',
+        header: '',
+        cell: ({ row }) => {
+            return (row.original.status === 'pending') ? h('div', { class: 'flex justify-end items-center gap-3' }, [
+                h(UButton, {
+                    color: 'success',
+                    icon: 'i-lucide-check',
+                    size: 'sm',
+                    class: 'cursor-pointer',
+                    label: 'Accepter',
+                    onClick: () => {
+                        return navigateTo({
+                            path: `/app/invoices/${row.original.id}`,
+                            query: route.query,
+                        })
+                    }
+                }),
+                h(UButton, {
+                    color: 'error',
+                    icon: 'i-lucide-x',
+                    size: 'sm',
+                    class: 'cursor-pointer',
+                    label: 'Refuser',
+                    onClick: async () => {
+                        selectedInvoices.value = [row.original.id]
+                        await onSubmit()
+                    }
+                }),
+            ]) : null
+        }
+    }
+]
 
 </script>
 
